03752871e3f5948567a6caac33f503011f6e9d72
yevhenii-moroziuk <yevhen.moroziuk@hackolade.com>
2024-12-03 18:32:50 +0200

    HCK-8971: include schema name in index name of DDL

--- forward_engineering/helpers/indexHelper.js
+++ forward_engineering/helpers/indexHelper.js
@@ -59,6 +59,16 @@ const getIndexKeys = (keys, jsonSchema, definitions) => {
 	};
 };
 
+const getIndexName = ({ name, dbName }) => {
+	const indexName = replaceSpaceWithUnderscore(name);
+
+	if (!indexName) {
+		return '';
+	}
+
+	return dbName ? `${dbName}.${name}` : name;
+};
+
 const getIndexes = (containerData, entityData, jsonSchema, definitions, areColumnConstraintsAvailable) => {
 	if (areColumnConstraintsAvailable) {
 		return '';
@@ -75,7 +85,7 @@ const getIndexes = (containerData, entityData, jsonSchema, definitions, areColum
 			const { columns, isIndexActivated = true } = getIndexKeys(indexData.SecIndxKey, jsonSchema, definitions);
 
 			return getIndexStatement({
-				name: replaceSpaceWithUnderscore(indexData.name),
+				name: getIndexName({ name: indexData.name, dbName }),
 				dbName: dbName,
 				tableName: tableName,
 				columns,
