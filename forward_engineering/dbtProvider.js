b868f4f297ac4faeb409b7826615da335a6bce73
Serhii Filonenko <91055067+serhii-filonenko@users.noreply.github.com>
2025-03-06 10:40:52 +0200

    HCK-10157: enable dbt feature (#108)
    * HCK-10157: enable dbt feature
    
    * HCK-10157: add dbt provider
    
    * HCK-10157: fix return type

---
+++ forward_engineering/dbtProvider.js
@@ -0,0 +1,49 @@
+/**
+ * @typedef {import('./types').ColumnDefinition} ColumnDefinition
+ * @typedef {import('./types').ConstraintDto} ConstraintDto
+ * @typedef {import('./types').JsonSchema} JsonSchema
+ */
+
+const columnHelper = require('./helpers/columnHelper');
+const constraintHelper = require('./helpers/constraintHelper');
+
+class DbtProvider {
+	/**
+	 * @returns {DbtProvider}
+	 */
+	static createDbtProvider() {
+		return new DbtProvider();
+	}
+
+	/**
+	 * @param {{ columnDefinition: ColumnDefinition }}
+	 * @returns {string}
+	 */
+	decorateType({ columnDefinition }) {
+		const type = columnHelper.getTypeByProperty([], '')(columnDefinition);
+		const isComplexType = /^(array|struct)/i.test(type);
+
+		return isComplexType ? type.replace(/<[\s\S]+>$/, '<>') : type;
+	}
+
+	/**
+	 * @param {{ jsonSchema: JsonSchema }}
+	 * @returns {ConstraintDto[]}
+	 */
+	getCompositeKeyConstraints({ jsonSchema }) {
+		const compositePrimaryKeys = constraintHelper.getCompositePrimaryKeys({ jsonSchema });
+		const compositeUniqueKeys = constraintHelper.getCompositeUniqueKeys({ jsonSchema });
+
+		return [...compositePrimaryKeys, ...compositeUniqueKeys];
+	}
+
+	/**
+	 * @param {{ columnDefinition: ColumnDefinition; jsonSchema: JsonSchema }}
+	 * @returns {ConstraintDto[]}
+	 */
+	getColumnConstraints({ columnDefinition, jsonSchema }) {
+		return constraintHelper.getColumnConstraints({ columnDefinition });
+	}
+}
+
+module.exports = DbtProvider;
