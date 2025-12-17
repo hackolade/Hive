564c4857bb0582a405a7ae60a9fb528af74d3802
Alik <alik.rahmonov@gmail.com>
2025-12-17 18:01:03 +0100

    fix: user should add quotes (#141)

--- forward_engineering/helpers/columnHelper.js
+++ forward_engineering/helpers/columnHelper.js
@@ -432,8 +432,7 @@ const getColumnConstraintsStatement = ({ collection, column }) => {
 	}
 
 	if (defaultValue) {
-		const value = typeof defaultValue === 'string' ? `'${defaultValue}'` : defaultValue;
-		result.inline = ` DEFAULT ${value}`;
+		result.inline = ` DEFAULT ${defaultValue}`;
 	}
 
 	if (check) {
