3b5762fe661a176a640e536cccd15a79e6fed0db
chulanovskyi <oleg.chulanovskyi@binary-studio.com>
2025-12-18 17:40:37 +0200

    fix: normalized name escaping

--- forward_engineering/helpers/tableHelper.js
+++ forward_engineering/helpers/tableHelper.js
@@ -79,7 +79,7 @@ const getPrimaryKeyStatement = (jsonSchema, keysNames, deactivatedColumnNames, i
 		rely,
 		enableSpecification: 'DISABLE',
 	});
-	const constraintNameStatement = constraintName ? `CONSTRAINT ${constraintName} ` : '';
+	const constraintNameStatement = constraintName ? `CONSTRAINT ${prepareName(constraintName)} ` : '';
 
 	const getStatement = keys => `${constraintNameStatement}PRIMARY KEY (${keys})${constraintOptsStatement}`;
 
