dc5ce9d10a2551cd21d59e992ece5d454ef4f14c
Alik <alik.rahmonov@gmail.com>
2025-12-22 13:30:53 +0100

    HCK-14051: fix SQL formatting (#165)
    * remove lib
    
    * fix formatting
    
    * HCK-14070: References require DB name if it exists
    
    * fix
    
    * keep minify
    
    * fix formatting, format/minify struct
    
    * fix
    
    * Fix commas and spaces
    
    ---------
    
    Co-authored-by: Vitalii Yarmus <mr.yarmus@gmail.com>

--- forward_engineering/helpers/tableHelper.js
+++ forward_engineering/helpers/tableHelper.js
@@ -46,18 +46,18 @@ const getCreateStatement = ({
 			.map(item => item + ' ')
 			.join('');
 	const fullTableName = dbName ? `${dbName}.${tableName}` : tableName;
-	const hasConstraint = primaryKeyStatement || uniqueKeyStatement || checkStatement;
+	const hasConstraint = primaryKeyStatement || uniqueKeyStatement || checkStatement || foreignKeyStatement;
 
 	return buildStatement(
 		`CREATE${tempExtStatement}TABLE ${ifNotExist ? 'IF NOT EXISTS ' : ''}${fullTableName} (`,
 		isActivated,
 	)(columnStatement, columnStatement + (hasConstraint ? ',' : ''))(
 		primaryKeyStatement,
-		primaryKeyStatement + (uniqueKeyStatement || checkStatement ? ',' : ''),
-	)(uniqueKeyStatement, uniqueKeyStatement + (checkStatement ? ',' : ''))(checkStatement, checkStatement)(
-		foreignKeyStatement,
-		foreignKeyStatement,
-	)(true, ')')(comment, `COMMENT '${encodeStringLiteral(comment)}'`)(
+		primaryKeyStatement + (uniqueKeyStatement || checkStatement || foreignKeyStatement ? ',' : ''),
+	)(uniqueKeyStatement, uniqueKeyStatement + (checkStatement || foreignKeyStatement ? ',' : ''))(
+		checkStatement,
+		checkStatement,
+	)(foreignKeyStatement, foreignKeyStatement)(true, ')')(comment, `COMMENT '${encodeStringLiteral(comment)}'`)(
 		partitionedByKeys,
 		`PARTITIONED BY (${partitionedByKeys})`,
 	)(clusteredKeys, `CLUSTERED BY (${clusteredKeys})`)(sortedKeys && clusteredKeys, `SORTED BY (${sortedKeys})`)(
@@ -82,7 +82,7 @@ const getPrimaryKeyStatement = (jsonSchema, keysNames, deactivatedColumnNames, i
 	});
 	const constraintNameStatement = constraintName ? `CONSTRAINT ${prepareName(constraintName)} ` : '';
 
-	const getStatement = keys => `${constraintNameStatement}PRIMARY KEY (${keys})${constraintOptsStatement}`;
+	const getStatement = keys => `${constraintNameStatement}PRIMARY KEY (${keys}) ${constraintOptsStatement}`.trim();
 
 	if (!Array.isArray(keysNames) || !keysNames.length) {
 		return '';
